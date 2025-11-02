1e1757df19518253f1c110a11376fccf72c06800
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-11-02 14:28:43 +0200

    Updates Jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 export default {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  testRegex: '(/dist/tests/.*|(\\.|/)(test))\\.[t]s?$',
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js',
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts',
+  ],
   verbose: true,
   coverageDirectory: __dirname + '/tests/coverage',
 };
