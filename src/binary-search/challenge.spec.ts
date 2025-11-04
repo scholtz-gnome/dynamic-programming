fc98f9574feb36fd84bcde2574d7da95b83bd7fd
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-11-04 22:01:56 +0200

    Complete binarySearch challenge

--- src/binary-search/challenge.spec.ts
+++ src/binary-search/challenge.spec.ts
@@ -1,7 +1,93 @@
-import { binarySearch } from './challenge';
+import { binarySearch } from './solution';
 
 describe('binarySearch()', () => {
   it('accepts an array and a value as arguments', () => {
     binarySearch([1], 1);
+  });
+
+  it('returns a number', () => {
+    const result = binarySearch([1], 1);
+
+    expect(typeof result).toEqual('number');
+  });
+
+  describe('When the value passed in exists in the array', () => {
+    describe('When passed an array of 1 item', () => {
+      it('returns 0', () => {
+        const result = binarySearch([1], 1);
+
+        expect(result).toEqual(0);
+      });
+    });
+
+    describe('When passed an array of 2 items', () => {
+      it('returns 0', () => {
+        const result = binarySearch([1, 2], 1);
+
+        expect(result).toEqual(0);
+      });
+    });
+
+    describe('When passed an array of 3 items', () => {
+      it('returns 0', () => {
+        const result = binarySearch([1, 2, 3], 2);
+
+        expect(result).toEqual(1);
+      });
+    });
+
+    describe('When passed an array of 4 items', () => {
+      it('returns 0', () => {
+        const result = binarySearch([1, 2, 3, 4], 3);
+
+        expect(result).toEqual(2);
+      });
+    });
+
+    describe('When passed an array of 6 items', () => {
+      it('returns 0', () => {
+        const result = binarySearch([1, 2, 3, 4, 5, 6], 2);
+
+        expect(result).toEqual(1);
+      });
+    });
+
+    describe('Various success tests', () => {
+      it('returns the index', () => {
+        {
+          const result = binarySearch(
+            [5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+            95
+          );
+
+          expect(result).toEqual(16);
+        }
+
+        {
+          const result = binarySearch(
+            [5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99],
+            10
+          );
+
+          expect(result).toEqual(2);
+        }
+      });
+    });
+  });
+
+  describe('When the value passed in does not exist in the array', () => {
+    it('returns -1', () => {
+      {
+        const result = binarySearch([1, 2, 3, 4, 5, 6, 7, 8], 9);
+
+        expect(result).toEqual(-1);
+      }
+
+      {
+        const result = binarySearch([1, 2, 3, 4, 5], 6);
+
+        expect(result).toEqual(-1);
+      }
+    });
   });
 });
--- src/binary-search/solution.ts
+++ src/binary-search/solution.ts
@@ -1,1 +1,29 @@
-export const binarySearch = () => {};
+export const binarySearch = (items: number[], value: number): number => {
+  let lowerBound = 0;
+  let upperBound = items.length - 1;
+  let pointer = calculateMidpoint(lowerBound, upperBound);
+
+  if (items[pointer] === value) {
+    return pointer;
+  }
+
+  while (lowerBound <= upperBound) {
+    if (items[pointer] === value) {
+      return pointer;
+    }
+
+    if (items[pointer] < value) {
+      lowerBound = pointer + 1;
+    } else {
+      upperBound = pointer - 1;
+    }
+
+    pointer = calculateMidpoint(lowerBound, upperBound);
+  }
+
+  return -1;
+};
+
+const calculateMidpoint = (lowerBound: number, upperBound: number): number => {
+  return Math.floor((upperBound + lowerBound) / 2);
+};
