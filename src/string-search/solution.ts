62127249c9403c0eea6cc55985f5132a5e22f969
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-11-05 22:20:04 +0200

    Refactors stringSearch

--- src/string-search/solution.ts
+++ src/string-search/solution.ts
@@ -2,13 +2,9 @@ export const stringSearch = (searchItem: string, substring: string): number => {
   let count = 0;
 
   for (let i = 0; i < searchItem.length; i++) {
-    let matchCount = 0;
-
     for (let j = 0; j < substring.length; j++) {
       if (searchItem[i + j] === substring[j]) {
-        matchCount++;
-
-        if (matchCount === substring.length) {
+        if (j === substring.length - 1) {
           count++;
         }
       } else {
